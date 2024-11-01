2a22d8ca4822477f0bde8d4c876d5a5f02995e05
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-11-01 16:28:17 +0900

    fix: Subscriber session_id를 가지도록 수정

--- src/main/java/nextstep/session/infrastructure/JdbcSubscriberRepository.java
+++ src/main/java/nextstep/session/infrastructure/JdbcSubscriberRepository.java
@@ -24,7 +24,7 @@ public class JdbcSubscriberRepository implements SubscriberRepository {
     public int save(Subscriber subscriber) {
         String sql = "insert into subscriber (session_id, ns_user_id, created_at, updated_at) values(?, ?, ?, ?)";
         return jdbcTemplate.update(sql,
-                subscriber.getSession().getId(),
+                subscriber.getSessionId(),
                 subscriber.getNsUser().getId(),
                 subscriber.getDateDomain().getCreatedAt(),
                 subscriber.getDateDomain().getUpdatedAt()
@@ -36,33 +36,12 @@ public class JdbcSubscriberRepository implements SubscriberRepository {
         String sql = "select id, session_id, ns_user_id, created_at, updated_at from subscriber where id = ?";
         RowMapper<Subscriber> rowMapper = (rs, rowNum) -> new Subscriber(
                 rs.getLong(1),
-                findSessionById(rs.getLong(2)),
+                rs.getLong(2),
                 findUserById(rs.getLong(3)),
                 toLocalDateTime(rs.getTimestamp(4)),
                 toLocalDateTime(rs.getTimestamp(5)));
 
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
-    }
-
-    private Session findSessionById(Long sessionId) {
-        String sql = "select id, title, paymentType, subscribeStatus, subscribeMax, price, start_date, end_date, image_name, image_width, image_height, image_capacity, created_at, updated_at from session where id = ?";
-        RowMapper<Session> rowMapper = (rs, rowNum) -> new Session(
-                rs.getLong(1),
-                rs.getString(2),
-                rs.getString(3),
-                rs.getString(4),
-                rs.getInt(5),
-                rs.getInt(6),
-                toLocalDateTime(rs.getTimestamp(7)),
-                toLocalDateTime(rs.getTimestamp(8)),
-                rs.getString(9),
-                rs.getInt(10),
-                rs.getInt(11),
-                rs.getInt(12),
-                toLocalDateTime(rs.getTimestamp(13)),
-                toLocalDateTime(rs.getTimestamp(14)));
-
-        return jdbcTemplate.queryForObject(sql, rowMapper, sessionId);
     }
 
     private NsUser findUserById(Long userId) {
