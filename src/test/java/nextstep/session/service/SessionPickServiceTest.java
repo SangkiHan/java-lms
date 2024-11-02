7869f36fd57a86a2866ab648d764468f2f0c0681
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-11-03 01:10:32 +0900

    test: 테스트코드

--- src/test/java/nextstep/session/service/SessionPickServiceTest.java
+++ src/test/java/nextstep/session/service/SessionPickServiceTest.java
@@ -1,5 +1,6 @@
 package nextstep.session.service;
 
+import nextstep.session.domain.ApproveStatus;
 import nextstep.session.domain.PickSession;
 import nextstep.session.domain.Session;
 import nextstep.session.domain.SessionStatus;
@@ -22,7 +23,7 @@ class SessionPickServiceTest extends TestSupport {
     @Autowired
     private SessionService sessionService;
 
-    @DisplayName("강의 승인자를 저장한다.")
+    @DisplayName("강의에 선발된 인원을 저장한다.")
     @Test
     void subscribeTest() {
         LocalDateTime startDate = LocalDateTime.parse("2023-04-05T00:00:00");
@@ -40,5 +41,26 @@ class SessionPickServiceTest extends TestSupport {
         assertThat(findSession.getSessionPicks().getSessionPicks())
                 .extracting("nsUser.id", "nsUser.userId", "nsUser.password", "nsUser.name", "nsUser.email")
                 .contains(tuple(1L, "javajigi", "test", "자바지기", "javajigi@slipp.net"));
+    }
+
+    @DisplayName("선발된 인원의 강의를 승인한다.")
+    @Test
+    void approveSessionPickTest() {
+        LocalDateTime startDate = LocalDateTime.parse("2023-04-05T00:00:00");
+        LocalDateTime endDate = LocalDateTime.parse("2023-05-05T00:00:00");
+
+        Image image = new Image(1L, "테스트이미지.jpg", 300, 200, 1);
+        Session session = Session.createPaid(1L, "테스트강의", List.of(image), PickSession.NON_PICK,1, 800000, startDate, endDate);
+
+        sessionService.save(session);
+
+        sessionService.changeSubscribeStatus(1L, SessionStatus.PROCESS);
+        sessionPickService.enrollPickUser(1L, 1L);
+
+        sessionPickService.approvePickUser(1L, ApproveStatus.APPROVED);
+
+        assertThat(sessionPickService.findById(1L))
+                .extracting("approveStatus")
+                .isEqualTo(ApproveStatus.APPROVED);
     }
 }
